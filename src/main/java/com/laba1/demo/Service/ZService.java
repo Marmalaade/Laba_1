42f68965f14e86acf34cd1471fe796be6b725a29
Marmallaade <sasha_i01@mail.ru>
2021-02-26 16:51:11 +0300

    Seconde commit

--- src/main/java/com/laba1/demo/Service/ZService.java
+++ src/main/java/com/laba1/demo/Service/ZService.java
@@ -1,17 +1,38 @@
 package com.laba1.demo.Service;
 
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
 
 public class ZService
 {
+
     private final int afterCollisionAnswer;
+
     private final int weight1;
     private final int speed1;
+
     private final int weight2;
+
     private final int speed2;
 
     public int getAfterCollisionAnswer() {
         return afterCollisionAnswer;
+    }
+
+    public int getWeight1() {
+        return weight1;
+    }
+
+    public int getSpeed1() {
+        return speed1;
+    }
+
+    public int getWeight2() {
+        return weight2;
+    }
+
+    public int getSpeed2() {
+        return speed2;
     }
 
     public ZService(int weight1, int speed1, int weight2, int speed2) {
